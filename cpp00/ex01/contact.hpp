295f1b2f8ffd6919ad1ea1daa2447311f3235e10
JDH <hotchkinj3@gmail.com>
2024-10-03 10:19:32 +0200

    chore: contact class

--- cpp00/ex01/contact.hpp
+++ cpp00/ex01/contact.hpp
@@ -5,16 +5,38 @@
 #include <string>
 
 typedef std::string str;
-# define out std::cout
-# define end std::endl
+# define cout std::cout
+# define endl std::endl
 
 class Contact
 {
-	public:
-	Contact()//default empty constructor
+public:
+	Contact();//default empty constructor
+	~Contact();//destructor
 
+	//getters
+	str	getFirstName() const;
+	str	getLastName() const;
+	str	getNickName() const;
+	str	getPhoneNumber() const;
+	str	getDarkestSecret() const;
 
+	//setters
+	void	setFirstName(const str &firstname);
+	void	setLastName(const str &lastname);
+	void	setNickName(const str &nickname);
+	void	setPhoneNumber(const str &number);
+	void	setDarkestSecret(const str &secret);
 
+private:
+	str first_name;
+	str last_name;
+	str nickname;
+	str phone_number;
+	str darkest_secret;
 };
 
 #endif
+
+/* fields:
+first name, last name, nickname, phone number, darkest secret. */
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -2,12 +2,18 @@
 # define PNONEBOOK_HPP
 
 #include <contact.hpp>
+#include <iostream>
+#include <string>
 
 class PhoneBook
 {
-	public:
+public:
+	PhoneBook();
+	~PhoneBook();
 
-
+	// add methods(function prototypes for add, etc... here)
+private:
+	Contact contacts[8];
 };
 
 
