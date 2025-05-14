bfa59bdc4ff6c77a2ee4988bbc19180547553090
JDH <hotchkinj3@gmail.com>
2025-05-14 22:28:58 +0200

    fix: fix out of bounds inconsistancy

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -3,11 +3,16 @@
 int main(int ac, char **av)
 {
 	if (ac == 2)
-	{
 		ScalarConverter::convert(av[1]);
-	}
 	else
 		std::cout << "Usage: ./converter <value>" << std::endl;
 
 	return (0);
 }
+/* 
+For testing non printalble chars..
+
+./converter $'\x1B'  ESC
+$'\x7F'  DEL
+$'\x00'  NUL
+$'\x07'  BELL */
--- cpp06/ex00/printType.cpp
+++ cpp06/ex00/printType.cpp
@@ -40,28 +40,6 @@ void	printChar(const std::string& in)
 	std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
 }
 
-void setDouble(const std::string& in)
-{
-	char	*end;
-	end = NULL;
-	errno = 0;
-
-	double val = std::strtod(in.c_str(), &end);
-	if (errno == ERANGE || val < DMIN || val > DMAX)
-	{
-		std::cout << "float : " << "impossible" << std::endl;
-		std::cout << "double: " << "impossible" << std::endl;
-	}
-	else
-	{
-		if (val < FMIN || val > FMAX)
-			std::cout << "float : " << "impossible" << std::endl;
-		else
-			std::cout << "float : " << static_cast<float>(val) << ".0f" << std::endl;
-		std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
-	}
-}
-
 void	printInt(const std::string& in)
 {
 	char	*end = NULL;	
@@ -72,6 +50,8 @@ void	printInt(const std::string& in)
 	{
 		std::cout << "char  : impossible" << std::endl;
 		std::cout << "int   : impossible" << std::endl;
+		std::cout << "float : impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
 	}
 	else
 	{
@@ -87,10 +67,10 @@ void	printInt(const std::string& in)
 			std::cout << "char  : '" << c << "'" << std::endl;
 		}
 		std::cout << "int   : " << i << std::endl;
+		std::cout << "float : " << static_cast<float>(val) << ".0f" << std::endl;
+		std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
 	}
-	setDouble(in);
 }
-
 
 void	printDouble(const std::string& in)
 {
@@ -150,25 +130,6 @@ void	printDouble(const std::string& in)
 	}
 }
 
-//performs check and set for the double type
-
-void	setDoub(const std::string& in)
-{
-	char	*end;
-	end = NULL;
-	errno = 0;
-
-	double val = std::strtod(in.c_str(), &end);
-
-	if (errno == ERANGE || val < DMIN || val > DMAX)
-		std::cout << "double: impossible" << std::endl;
-	else if (fabs(val) - std::floor(fabs(val)) < .000005)
-		std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
-	else
-		std::cout << "double: " << static_cast<double>(val) << std::endl;
-}
-
-
 void	printFloat(const std::string& in)
 {
 	char	*end;
@@ -181,7 +142,7 @@ void	printFloat(const std::string& in)
 		std::cout << "char  : impossible" << std::endl;
 		std::cout << "int   : impossible" << std::endl;
 		std::cout << "float : impossible" << std::endl;
-		setDoub(in);
+		std::cout << "double: impossible" << std::endl;
 	}
 	else if (val < IMIN || val > IMAX)
 	{
@@ -223,4 +184,3 @@ void	printFloat(const std::string& in)
 		}
 	}
 }
-
