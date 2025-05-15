915ad9e7962f430de817e6d33cfb2bc7a5880353
JDH <hotchkinj3@gmail.com>
2025-05-15 22:48:55 +0200

    remove old comment out

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -52,37 +52,4 @@ void	ScalarConverter::convert(const std::string& in)
 			std::cout << "Invalid entry" << std::endl;
 			break ;
 	}
-	
-	
-	
-
-	//char
-	
-	
-
-	/* //int
-	
-	int i = static_cast<int>(val);
-
-	if (i < std::numeric_limits<int>::min() || i > std::numeric_limits<int>::max())
-		std::cout << "Int: Not representable within limits of type 'int'." << std::endl;
-	else
-		std::cout << "Int: " << i << std::endl;
-
-	//float
-
-	float f = static_cast<float>(val);
-
-	if (f < std::numeric_limits<float>::min() || f > std::numeric_limits<float>::max())
-		std::cout << "Float: Not representable within limits of type 'float'." << std::endl;
-	else
-		std::cout << "Float: " << f << std::endl;
-
-
-	//double
-
-	if (val < std::numeric_limits<double>::min() || val > std::numeric_limits<double>::max())
-		std::cout << "Double: Not representable within limits of type 'double'." << std::endl;
-	else
-		std::cout << "Double: " << val << std::endl; */
 }
